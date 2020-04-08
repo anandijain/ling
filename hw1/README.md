97171547d75721e0c1e8685caaa6b1dc762048ca
anandijain <anandj@uchicago.edu>
2020-04-08 15:52:25 -0500

    hw1 for comp ling

---
+++ hw1/README.md
@@ -0,0 +1,11 @@
+# hw1 comp ling
+
+# anagrams.py
+
+methods:
+	* sort(s:str): returns alphabetized string
+	* parse(s:str): takes in a string and removes `\n`, converts to lowercase,
+	* anagrams_count(l:list): counts occurences of alaphabetized words in a list of words, returning a dictionary where each key is an alphabetized word, and the value is the number of occurences.
+	* anagrams(l:list): creates a dictionary with alphabetized words as keys, and all occurences of anagrams in sorted order as the value
+	* get(d:dict, s:str): given the anagrams_dict from anagrams() and a lookup str: returns the anagrams of that word
+	* 
---
+++ hw1/anagrams.py
@@ -0,0 +1,89 @@
+# Anand Jain
+# todo: min word len
+import pandas as pd
+
+""" 
+notes:
+
+pandas used for outside of assignment work to find most common anagrams
+
+"""
+
+def sort(s: str) -> str:
+    # returns alphabetized string
+    return ''.join(sorted(s))
+
+
+def parse(s: str) -> str:
+    # removes `\n` and uppercases
+    return s.strip().lower()
+
+
+def anagrams_count(l: list) -> dict:
+    # like anagrams(), but counts
+    d = {}
+    for elt in l:
+        alphabetized = sort(elt)
+        try:
+            d[alphabetized] += 1
+        except KeyError:
+            d[alphabetized] = 1
+    return d
+
+
+"""
+anagrams(l:list): creates a dictionary with alphabetized words as keys, 
+    the value is a 2-tuple:
+        [0] is all occurences of anagrams in sorted order
+        [1] is the count 
+
+"""
+def anagrams(l: list) -> dict:
+    d = {}
+    for elt in l:
+        alphabetized = sort(elt)
+        try:
+            if alphabetized not in d[alphabetized][0]:
+                d[alphabetized][0].append(elt)
+                d[alphabetized][1] += 1
+        except KeyError:
+            d[alphabetized] = [[elt], 1]
+    return d
+
+
+"""
+quick fxn to get the anagrams of a word, given the anagram dictionary
+and a word to lookup
+
+assuming word is parsed (lowercase and no `\n`)
+"""
+def get(d: dict, s: str):
+    alphabetized = sort(s)
+    return d.get(alphabetized)
+
+
+"""
+io function to read in words file
+
+"""
+def get_words() -> list:
+    f = open("english-words-235k.txt", "r")
+    lines = f.readlines()
+    return sorted(set(map(parse, lines)))
+
+
+def main_test(test_str: str):
+    lines = get_words()
+    anagrams_dict = anagrams(lines)
+#    for k, v in anagrams_dict.items():
+#        print(f'{k}: {len(v)} {v} \n')
+
+    print(get(anagrams_dict, test_str))
+
+def make_df():
+    d = anagrams(get_words())
+    df = pd.DataFrame.from_dict(d, orient='index', columns=['anagrams', 'count'])
+    return df
+
+if __name__ == "__main__":
+    main_test('agnor')
