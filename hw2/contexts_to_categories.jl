4ba2e78b77147823e2711b4c64f0b2158de24a2c
anandijain <anandj@uchicago.edu>
2020-06-12 21:55:06 -0500

    fmt

--- hw2/contexts_to_categories.jl
+++ hw2/contexts_to_categories.jl
@@ -41,46 +41,56 @@ end
 
 dust(d::Dict{String,Dict{String,Int}}, ; n = 50) = filter(x -> sum(values(last(x))) > n, d)
 
-# write this out 
+# write this out
 context_list(ctxs)::AbstractDataFrame = sort(
     DataFrame(:cx => collect(keys(ctxs)), :wc => length.(values(ctxs))),
     :wc,
     rev = true,
 )
 
-contexts_to_dfs(ctxs)::Dict{String, DataFrame} = Dict(zip(collect(keys(ctxs)), map(
-    x -> sort(DataFrame(:w => collect(keys(x)), :n => collect(values(x))), :n, rev = true),
-	values(ctxs)))
-)
+contexts_to_dfs(ctxs)::Dict{String,DataFrame} = Dict(zip(
+    collect(keys(ctxs)),
+    map(
+        x -> sort(
+            DataFrame(:w => collect(keys(x)), :n => collect(values(x))),
+            :n,
+            rev = true,
+        ),
+        values(ctxs),
+    ),
+))
 
 function words_in_contexts(df_dict::Dict{String,DataFrame}; n = 5)
     rs = []
     for (ctx, df) in df_dict
         if nrow(df) < n
             r = vcat(ctx, collect.(zip(df.w[1:end], df.n[1:end]))...)
-			r = vcat(r, fill(0, 2n + 1 - length(r)))
+            r = vcat(r, fill(0, 2n + 1 - length(r)))
             push!(rs, r)
         else
             r = vcat(ctx, collect.(zip(df.w[1:5], df.n[1:5]))...)
             push!(rs, r)
         end
     end
-	ns = vcat("context", collect(Iterators.flatten(zip("word_" .* string.(1:5), "n_" .* string.(1:5)))))
-	clean_sort_wics(DataFrame(permutedims(hcat(rs...)), Symbol.(ns)))
-	# write this out
+    ns = vcat(
+        "context",
+        collect(Iterators.flatten(zip("word_" .* string.(1:5), "n_" .* string.(1:5)))),
+    )
+    clean_sort_wics(DataFrame(permutedims(hcat(rs...)), Symbol.(ns)))
+    # write this out
 end
 
-function clean_sort_wics(df) 
-	df[:, r"n_\d"] = Int.(df[:, r"n_\d"])
-	df.sum = sum.(eachrow(df[:, r"n_\d"]))
-	sort(df, :sum, rev=true)
+function clean_sort_wics(df)
+    df[:, r"n_\d"] = Int.(df[:, r"n_\d"])
+    df.sum = sum.(eachrow(df[:, r"n_\d"]))
+    sort(df, :sum, rev = true)
 end
 
 function assignment()
-	cs = dust(get_contexts())
-	cl = context_list(cs)
-	wics = words_in_contexts(contexts_to_dfs(cs))
-	cl, wics
+    cs = dust(get_contexts())
+    cl = context_list(cs)
+    wics = words_in_contexts(contexts_to_dfs(cs))
+    cl, wics
 end
 
 export get_lines, get_contexts, context_list, dust, contexts_to_dfs, words_in_contexts
